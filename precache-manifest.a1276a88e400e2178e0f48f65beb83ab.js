67bf71387bf440f9fea2801ab83bab70e4d02b83
Shawn Zhou <feshawnzhou@gmail.com>
2022-01-14 13:52:56 -0800

    deploy

---
+++ precache-manifest.a1276a88e400e2178e0f48f65beb83ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f11a0c161780bbc143508881d36ace",
+    "url": "/development-test/index.html"
+  },
+  {
+    "revision": "b4ed3ac55c6d0d130fdb",
+    "url": "/development-test/static/css/main.f96ef3b1.chunk.css"
+  },
+  {
+    "revision": "998118cf48fb5d8da5f7",
+    "url": "/development-test/static/js/2.32c55941.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/development-test/static/js/2.32c55941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ed3ac55c6d0d130fdb",
+    "url": "/development-test/static/js/main.18e9e3e2.chunk.js"
+  },
+  {
+    "revision": "1380d6c88bbf5b96b4d3",
+    "url": "/development-test/static/js/runtime-main.a5f1a0e2.js"
+  },
+  {
+    "revision": "be839b56d61688e12cf0d8235c092062",
+    "url": "/development-test/static/media/leftArrow.be839b56.svg"
+  },
+  {
+    "revision": "6df5f90d1e8b34d794f6b9bc3bd0e390",
+    "url": "/development-test/static/media/rightArrow.6df5f90d.svg"
+  }
+]);
